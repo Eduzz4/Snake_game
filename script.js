b9d804848d9730728a4d8c326f5e85a5299b9e6c
Eduardo <eduardo.s.luciano1@gmail.com>
2026-08-13 12:57:57 -0300

    Adiciona endereco de coordenadas (x, y) no grid

--- script.js
+++ script.js
@@ -4,11 +4,32 @@ const btnIniciar = document.querySelector(".iniciar-game");
 let totalDeCasas = 144;
 
 function criarTabuleiro() {
+  let listaDeDivs = [];
   for (let i = 0; i < totalDeCasas; i++) {
-    const celulas = document.createElement("div");
-    celulas.classList.add("celula");
-    tabuleiro.appendChild(celulas);
+    const celula = document.createElement("div");
+    celula.classList.add("celula");
+    listaDeDivs.push(celula);
+    tabuleiro.appendChild(celula);
   }
+  enderecarGrid(listaDeDivs);
+}
+
+function enderecarGrid(div) {
+  let x = 0;
+  let y = 0;
+
+  div.forEach((elemento) => {
+    if (x < 11) {
+      elemento.dataset.x = x;
+      elemento.dataset.y = y;
+      x += 1;
+    } else {
+      elemento.dataset.x = x;
+      elemento.dataset.y = y;
+      x = 0;
+      y += 1;
+    }
+  });
 }
 
 criarTabuleiro();
